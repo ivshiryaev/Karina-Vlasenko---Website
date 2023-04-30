ee008bf4fae82b8dc957a1c53a1d08bd5250d07f
Ivan Shyriaiev <ivshiryaev1999@gmail.com>
2023-04-30 21:04:17 +0200

    animations done

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ const observer = new IntersectionObserver((entries) => {
 		{
 			entry.target.classList.add('show');
 		}else{
-			// entry.target.classList.remove('show');
+			// entry.target.classList.remove('show');	
 		}
 	});
 });
